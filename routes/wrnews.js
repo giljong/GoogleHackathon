703f798c441f53c313c991511215bd02c4cdf3bd
giljong <minjong5054@daum.net>
2019-04-20 00:49:13 +0900

    ddd

--- routes/wrnews.js
+++ routes/wrnews.js
@@ -1,1 +1,10 @@
 //기사 작성
+const express = require('express');
+const db = require('../db/connetion');
+const router = express.Router();
+
+router.get('/',(req,res) => {
+    res.render('wrnews.ejs');
+})
+
+module.exports = router;
